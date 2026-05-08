05c05c9fe4e09333967b28e85bedea1f520715e1
SBermejoM01 <sbermejom01@educarex.es>
2026-05-08 20:18:58 +0200

    Optimización de imagenes

--- init_db.js
+++ init_db.js
@@ -8,7 +8,7 @@ async function initDb() {
         email VARCHAR(255) UNIQUE NOT NULL,
         username VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
-        avatar VARCHAR(255) DEFAULT 'avatar_default.png'
+        avatar VARCHAR(255) DEFAULT 'avatar_default.webp'
       );
     `);
         console.log('Table "users" created successfully or already exists.');
